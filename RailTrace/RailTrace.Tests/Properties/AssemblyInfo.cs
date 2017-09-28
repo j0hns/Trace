7815ef0d07ebb684c0b070ac8532108144d9239e
john <john>
2017-09-28 18:17:59 +0100

    Added test proj

---
+++ RailTrace/RailTrace.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RailTrace.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("BP")]
+[assembly: AssemblyProduct("RailTrace.Tests")]
+[assembly: AssemblyCopyright("Copyright © BP 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1041f0d7-8280-4b53-a653-b32bcaec6db9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RailTrace/RailTrace/RailHistoricServicePerformance.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RailTrace
+{
+    public class RailHistoricServicePerformance
+    {
+        private string url = "https://hsp-prod.rockshore.net/";
+
+
+
+
+        private T GetData<T,TPAYLOAD>(TPAYLOAD payload)
+        {
+            var payloadJson= JsonConvert.SerializeObject(payload);
+            using (var webClient=new WebClient())
+            {
+                webClient.Headers["Content-Type"] = "application/json";
+                //Authorization: Basic {Base64("email:password")}
+                var data =webClient.UploadString(url, payloadJson);
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+        }
+
+        public class ServiceMetrics
+        {
+            [JsonProperty("from_loc")]
+            public string FromLoc { get; set; }
+
+            [JsonProperty("to_loc")]
+            public string ToLoc { get; set; }
+
+            [JsonProperty("from_time")]
+            public string FromTime { get; set; }
+
+            [JsonProperty("to_time")]
+            public string ToTime { get; set; }
+
+            [JsonProperty("from_date")]
+            public string FromDate  { get; set; }
+
+            [JsonProperty("to_date")]
+            public string ToDate { get; set; }
+
+            [JsonProperty("days")]
+            public string Days { get; set; }
+        }
+
+    }
+}
