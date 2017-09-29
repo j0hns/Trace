877ac5ae5bf7b50290cf63db5d54daa06327743e
John Scully <john.scully@talk21.com>
2017-09-30 00:25:41 +0100

    Resolves MAC Addresses and Destination IP Addresses

--- GeoTrace/GeoTrace/MainWindowViewModel.cs
+++ GeoTrace/GeoTrace/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -18,6 +20,9 @@ namespace GeoTrace
         private readonly Map _map;
         private MapLayer _pinLayer;
         private string _routerUri;
+        private List<KnownDevice> _knownDevices = new List<KnownDevice>();
+        private Dictionary<string, KnownDevice> _ipToMac = new Dictionary<string, KnownDevice>();
+
 
         public MainWindowViewModel(Map map)
         {
@@ -45,6 +50,9 @@ namespace GeoTrace
             public string DestinationIpAddress { get; set; }
             public string DestinationPort { get; set; }
             public string Status { get; set; }
+            public string SourceName { get; set; }
+            public string SourceMAC { get; set; }
+            public string DestinationName { get; set; }
         }
         public async Task<List<TraceRouteInfo>> GetConnectionInfo()
         {
@@ -54,18 +62,84 @@ namespace GeoTrace
                 var r = await client.TryLoginAsync(ConfigurationManager.AppSettings["Username"], ConfigurationManager.AppSettings["Password"], 5000);
 
 
-                var connectionList= await GetIpConnections(client);
+                var connectionList = await GetIpConnections(client);
 
 
-                var client_info_tmp = await ExecuteCommandAsync(client, "nvram get client_info_tmp");
-                var client_info_tmp_clean = client_info_tmp.Split(',');
-                var custom_clientlist = await ExecuteCommandAsync(client, "nvram get custom_clientlist");
 
+                var customList = (await ExecuteCommandAsync(client, "nvram get custom_clientlist")).Split('<');
+                var asusDevice = (await ExecuteCommandAsync(client, "nvram get asus_device_list")).Split('<');
+                var fromDHCPLease = (await ExecuteCommandAsync(client, "cat /var/lib/misc/dnsmasq.leases")).Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+                var client_info_tmp = (await ExecuteCommandAsync(client, "nvram get client_info_tmp")).Split(',');
                 //nmp_custom_clientlist give list of all clients that have been connected?
-                var nmp_custom_clientlist = await ExecuteCommandAsync(client, "cat /jffs/nmp_client_list");
+                var nmp_custom_clientlist = (await ExecuteCommandAsync(client, "cat /jffs/nmp_client_list")).Split('<');
 
-                var dnsmasq_leases = await ExecuteCommandAsync(client, "cat /var/lib/misc/dnsmasq.leases");
-               
+                foreach (var item in fromDHCPLease)
+                {
+                    var parts = item.Split(' ');
+                    var mac = parts[1].ToLower().Replace(":", "");
+                    var ip = parts[2];
+                    var name = parts[3];
+
+                    var knownDevice = _knownDevices.FirstOrDefault(x => x.MAC == mac);
+                    if (knownDevice == null)
+                    {
+                         knownDevice = new KnownDevice { MAC = mac, Name = name };
+                        _knownDevices.Add(knownDevice);
+                    }
+                    AddIpToMac(ip, knownDevice);
+                }
+
+                foreach (var item in client_info_tmp.Where(x => x.Trim() != ""))
+                {
+                    var parts = item.Split('>');
+                    var name = parts[1];
+                    var ip = parts[2];
+                    var mac = parts[3].ToLower().Replace(":", "");
+
+                    var knownDevice = _knownDevices.FirstOrDefault(x => x.MAC == mac);
+                    if (knownDevice==null)
+                    {
+                        knownDevice = new KnownDevice { MAC = mac, Name = name };
+                        
+                        _knownDevices.Add(knownDevice);
+                    }
+
+                    AddIpToMac(ip, knownDevice);
+
+                }
+
+                foreach (var item in customList.Where(x=>x.Trim()!=""))
+                {
+                    var parts = item.Split('>');
+                    var name = parts[0];
+                    var mac = parts[1].ToLower().Replace(":", "");
+
+                    var knownDevice = _knownDevices.FirstOrDefault(x => x.MAC== mac);
+                    if (knownDevice == null)
+                    {
+                        knownDevice= new KnownDevice { MAC = mac, Name = name };
+                        _knownDevices.Add(knownDevice);
+                    }
+
+                }
+
+                foreach (var item in nmp_custom_clientlist.Where(x => x.Trim() != ""))
+                {
+                    var parts = item.Split('>');
+                    var name = parts[2];
+                    var mac = parts[0].ToLower().Replace(":", "");
+
+                    var knownDevice = _knownDevices.FirstOrDefault(x => x.MAC == mac);
+                    if (knownDevice == null)
+                    {
+                        knownDevice = new KnownDevice { MAC = mac, Name = name };
+                        _knownDevices.Add(knownDevice);
+                    }
+
+                }
+
+
+
 
                 return connectionList;
             }
@@ -73,15 +147,45 @@ namespace GeoTrace
 
         }
 
-        private static async Task<string> ExecuteCommandAsync(Client client,string command)
+        private void AddIpToMac(string ip, KnownDevice device)
+        {
+            if (_ipToMac.ContainsKey(ip))
+                _ipToMac.Remove(ip);
+
+            _ipToMac.Add(ip, device);
+        }
+
+        public class KnownDevice
+        {
+            public string MAC { get; set; }
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name} - {MAC}";
+            }
+        }
+
+        private static async Task<string> ExecuteCommandAsync(Client client, string command)
         {
             await client.WriteLine(command);
-            return await client.ReadAsync(TimeSpan.FromSeconds(20));
+            var result = await client.ReadAsync(TimeSpan.FromSeconds(20));
+            //remove first line
+            var firstNewLineIndex = result.IndexOf("\r\n");
+            result = result.Remove(0, firstNewLineIndex + 2);
+            //var lines = result.Split(new []{"\r\n"},StringSplitOptions.RemoveEmptyEntries);
+            //return lines.Skip(1).Take(lines.Length - 2).ToList();
+            result = result.Replace("admin@RT-N66U:/tmp/home/root#", "").Trim();
+
+
+            return result;
         }
+
+     
 
         private static async Task<List<TraceRouteInfo>> GetIpConnections(Client client)
         {
-           
+
             string s = await ExecuteCommandAsync(client, "netstat-nat -no");
             var lines = s.Split('\r');
 
@@ -95,11 +199,24 @@ namespace GeoTrace
                 {
                     Protocol = lineParts[0],
                     SourceIpAddress = sourceAddressParts[0],
-                    SourcePort = sourceAddressParts[1],
+                    SourcePort = sourceAddressParts.Length > 1 ? sourceAddressParts[1] : null,
                     DestinationIpAddress = destinationAddressParts[0],
-                    DestinationPort = destinationAddressParts[1],
+                    DestinationPort = destinationAddressParts.Length > 1 ? destinationAddressParts[1] : null,
                     Status = lineParts[3]
                 };
+                try
+                {
+                    var ipDestination = Dns.GetHostEntry(connection.DestinationIpAddress);
+                    connection.DestinationName = ipDestination.HostName;
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine($"Error on resolving IP address {connection.DestinationIpAddress}" + e);
+                 
+                }
+               
+                //var ipSource = Dns.GetHostEntry(connection.SourceIpAddress);
+               // connection.SourceName= ipSource.HostName;
                 connections.Add(connection);
             }
 
@@ -117,28 +234,48 @@ namespace GeoTrace
                 foreach (var connection in connections)
                 {
 
-
-                    // Replace "City" with the appropriate method for your database, e.g.,
-                    // "Country".
-                    city = reader.City(connection.DestinationIpAddress);
-
-                    var cityLocation = city.Location;
-                    var cityName = city.City.Name;
-
-                    if (cityName != null && cityLocation != null && cityLocation.HasCoordinates)
+                    try
                     {
 
-                        Pushpin pin = new Pushpin
+
+                        // Replace "City" with the appropriate method for your database, e.g.,
+                        // "Country".
+                        city = reader.City(connection.DestinationIpAddress);
+
+                        var cityLocation = city.Location;
+                        var cityName = city.City.Name;
+
+                        if (cityName != null && cityLocation != null && cityLocation.HasCoordinates)
                         {
-                            Location = new Location(cityLocation.Latitude.Value, cityLocation.Longitude.Value)
-                        };
-                        _pinLayer.AddChild(new TextBox() { Text = cityName }, pin.Location);
-                        // Adds the pushpin to the map.
-                        _map.Children.Add(pin);
+
+                            Pushpin pin = new Pushpin
+                            {
+                                Location = new Location(cityLocation.Latitude.Value, cityLocation.Longitude.Value)
+                            };
+
+                            KnownDevice sourceDevice = null;
+                            string sourceText = "Unknown Source";
+                            if (_ipToMac.TryGetValue(connection.SourceIpAddress, out sourceDevice))
+                            {
+                                sourceText = sourceDevice.Name;
+                            }
+
+
+
+                            _pinLayer.AddChild(new TextBox() { Text = $"'{sourceText}'\r\nto\r\n'{cityName}' ({connection.DestinationName})\r\n[{connection.Status}]" }, pin.Location);
+                            // Adds the pushpin to the map.
+                            _map.Children.Add(pin);
+                        }
+                        else
+                        {
+                            //unknown city
+                            Debug.WriteLine($"Failed to lookup IP address {connection.SourceIpAddress}");
+                        }
                     }
-                    else
+                    catch (Exception e)
                     {
-                        //unknown city
+                        Debug.WriteLine($"Error on lookup IP address {connection.SourceIpAddress}" + e);
+
                     }
                 }
             }
