ca4f8c4e2229e3adc59cb14b209d76f6a1c40f91
john <john>
2017-09-29 09:10:33 +0100

    Succesfully makes a request

---
+++ RailTrace/RailTrace.Tests/RailHistoricServicePerformanceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RailTrace.Tests
+{
+    [TestClass]
+    public class RailHistoricServicePerformanceTests
+    {
+        [TestMethod]
+        public void GetData() { 
+        //{
+        //    "from_loc":"BTN",
+        //    "to_loc":"VIC",
+        //    "from_time":"0700",
+        //    "to_time":"0800",
+        //    "from_date":"2016-07-01",
+        //    "to_date":"2016-08-01",
+        //    "days":"WEEKDAY"
+        var serviceMetricsRequest = new RailHistoricServicePerformance.Request()
+        {
+            Days = "WEEKDAY",
+            FromDate = "2017-09-01",
+            ToDate = "2017-09-29",
+            FromTime = "0600",
+            ToTime = "0900",
+            FromLoc = "IPS",
+            ToLoc = "SRA"
+        };
+
+            var request = RailHistoricServicePerformance.GetData(serviceMetricsRequest);
+
+        }
+    }
+}
--- RailTrace/RailTrace/RailHistoricServicePerformance.cs
+++ RailTrace/RailTrace/RailHistoricServicePerformance.cs
@@ -1,34 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace RailTrace
 {
-    public class RailHistoricServicePerformance
+    public static class RailHistoricServicePerformance
     {
-        private string url = "https://hsp-prod.rockshore.net/";
+       
 
+        
 
-
-
-        private T GetData<T,TPAYLOAD>(TPAYLOAD payload)
+        public static Response GetData(Request request)
         {
-            var payloadJson= JsonConvert.SerializeObject(payload);
+            var payloadJson= JsonConvert.SerializeObject(request);
+            var url = ConfigurationManager.AppSettings["ServiceMetricsUrl"];
+            var user = ConfigurationManager.AppSettings["UserEmail"];
+            var pwd = ConfigurationManager.AppSettings["UserPwd"];
+            var authenticationHeaderContents= Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes($"{user}:{pwd}"));
+
+
             using (var webClient=new WebClient())
             {
+              
                 webClient.Headers["Content-Type"] = "application/json";
-                //Authorization: Basic {Base64("email:password")}
+                webClient.Headers["Authorization"] = $"Basic {authenticationHeaderContents}";
+              
                 var data =webClient.UploadString(url, payloadJson);
-                return JsonConvert.DeserializeObject<T>(data);
+                return JsonConvert.DeserializeObject<Response>(data);
             }
         }
 
-        public class ServiceMetrics
+        public class Request
         {
+            
             [JsonProperty("from_loc")]
             public string FromLoc { get; set; }
 
@@ -49,7 +60,104 @@ namespace RailTrace
 
             [JsonProperty("days")]
             public string Days { get; set; }
+
+
+            [JsonProperty("toc_filter1")]
+            public string toc_filter1 { get; set; }
+
+            [JsonProperty("tolerance2")]
+            public double tolerance2 { get; set; }
         }
 
+        public class Response
+        {
+            public class ResponseHeader
+            {
+                [JsonProperty("from_loc")]
+                public string FromLoc { get; set; }
+
+                [JsonProperty("to_loc")]
+                public string ToLoc { get; set; }
+
+            }
+
+            public class ResponseService
+            {
+                public class ResponseServiceAttributesMetrics
+                {
+                    //    "origin_location": "BTN",
+                    //    "destination_location": "VIC",
+                    //    "gbtt_ptd": "0712",
+                    //    "gbtt_pta": "0823",
+                    //    "toc_code": "GX",
+                    //    "matched_services": "22",
+                    //    "rids": [ "201607013361753", "201607043443704", ... ]
+
+                    [JsonProperty("origin_location")]
+                    public string OriginLocation { get; set; }
+
+                    [JsonProperty("destination_location")]
+                    public string DestinationLocation { get; set; }
+
+                    [JsonProperty("gbtt_ptd")]
+                    public string DepartureTime { get; set; }
+
+                    [JsonProperty("gbtt_pta")]
+                    public string ArrivalTime { get; set; }
+
+                    [JsonProperty("toc_code")]
+                    public string TrainOperatorCode { get; set; }
+
+                    [JsonProperty("matched_services")]
+                    public string MatchedServices { get; set; }
+
+                    [JsonProperty("rids")]
+                    public string[] TrainJourneyIds { get; set; }
+
+
+                }
+
+                public class ResponseMetrics
+                {
+                    //    "Metrics": [
+                    //{
+                    //    "tolerance_value": "0",
+                    //    "num_not_tolerance": "5",
+                    //    "num_tolerance": "17",
+                    //    "percent_tolerance": "77",
+                    //    "global_tolerance": true
+                    //}
+
+                    [JsonProperty("tolerance_value")]
+                    public string ToleranceValue { get; set; }
+
+                    [JsonProperty("num_not_tolerance")]
+                    public string TrainsOutsideToleranceValue { get; set; }
+
+                    [JsonProperty("num_tolerance")]
+                    public string TrainsInsideToleranceValue { get; set; }
+
+
+                    [JsonProperty("percent_tolerance")]
+                    public string PercentTolerance { get; set; }
+
+                    [JsonProperty("global_tolerance")]
+                    public bool GlobalTolerance { get; set; }
+                }
+
+                public ResponseServiceAttributesMetrics ServiceAttributesMetrics { get; set; }
+                public ResponseMetrics Metrics { get; set; }
+            }
+
+           
+
+
+
+
+            public ResponseHeader Header { get; set; }
+            public ResponseService[] Services { get; set; }
+        }
+
+        
     }
 }
